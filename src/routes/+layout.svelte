90ceff4806a8af8bfeecd8db3dfe1400f3cdac8d
s0rus <piotrmolkontakt@gmail.com>
2023-02-25 14:22:33 +0100

    :sparkles: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+</script>
+
+<main class="h-screen w-screen">
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div
+	class="container mx-auto flex h-full w-full flex-col items-center justify-center space-y-8 p-8"
+>
+	<h1>Project D.</h1>
+	<p>Good luck and have fun!</p>
+
+	<label class="label">
+		<span>And this is a random input that does nothing.</span>
+		<input class="input px-4 py-2" />
+	</label>
+</div>
